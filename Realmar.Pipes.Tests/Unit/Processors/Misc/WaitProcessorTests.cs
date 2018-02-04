6240685866735001beedbe702ba22c88e2458183
Anastassios Martakos <anastassios.martakos@gmail.com>
2018-02-04 15:34:22 +0100

    Add WaitProcessor unit tests

---
+++ Realmar.Pipes.Tests/Unit/Processors/Misc/WaitProcessorTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Realmar.Pipes.Processors.Misc;
+using Xunit;
+
+namespace Realmar.Pipes.Tests.Unit.Processors.Misc
+{
+    public class WaitProcessorTests
+    {
+        [Fact]
+        public void Process_CheckTime()
+        {
+            var waitTime = 500;
+            var processor = new WaitProcessor<object>(waitTime);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            processor.Process(new object());
+            stopwatch.Stop();
+
+            Assert.True(stopwatch.Elapsed.TotalMilliseconds < waitTime * 1.1);
+        }
+    }
+}
