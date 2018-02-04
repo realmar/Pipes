e0692a6c5e67fd3db957cf45dde1b46d2eb34c6b
Anastassios Martakos <anastassios.martakos@gmail.com>
2018-02-04 15:32:01 +0100

    Add unit tests for CastProcessor

---
+++ Realmar.Pipes.Tests/Unit/Processors/Misc/CastProcessorTests.cs
@@ -0,0 +1,63 @@
+﻿using Realmar.Pipes.Processors.Misc;
+using Xunit;
+
+namespace Realmar.Pipes.Tests.Unit.Processors.Misc
+{
+    public class CastProcessorTests
+    {
+        private struct ExampleStruct { }
+        private class ExampleClass { }
+
+        private interface IExample { }
+        private class ExampleConcrete : IExample { }
+
+        [Fact]
+        public void Process_ValueType()
+        {
+            var processor = new CastProcessor<ExampleStruct>();
+            var data = new ExampleStruct() as object;
+
+            // implicit "DoesNotThrow" assertion
+            var result = processor.Process(data);
+        }
+
+        [Fact]
+        public void Process_ReferenceType()
+        {
+            var processor = new CastProcessor<ExampleClass>();
+            var data = new ExampleClass() as object;
+
+            var result = processor.Process(data);
+            Assert.IsType<ExampleClass>(result);
+        }
+
+        [Fact]
+        public void Process_DoubleToInt()
+        {
+            var processor = new CastProcessor<int>();
+            var result = processor.Process(2.78934d);
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Process_InterfaceToConcrete()
+        {
+            var processor = new CastProcessor<ExampleConcrete>();
+            IExample data = new ExampleConcrete();
+            var result = processor.Process(data);
+
+            Assert.IsType<ExampleConcrete>(result);
+        }
+
+        [Fact]
+        public void Process_ConcreteToInterface()
+        {
+            var processor = new CastProcessor<IExample>();
+            var data = new ExampleConcrete();
+            var result = processor.Process(data);
+
+            Assert.IsAssignableFrom<IExample>(result);
+        }
+    }
+}
--- Realmar.Pipes/Processors/Misc/CastProcessor.cs
+++ Realmar.Pipes/Processors/Misc/CastProcessor.cs
@@ -1,9 +1,17 @@
-﻿namespace Realmar.Pipes.Processors.Misc
+﻿using System;
+
+namespace Realmar.Pipes.Processors.Misc
 {
     public class CastProcessor<T> : IPipeProcessor<object, T>
     {
         public T Process(object data)
         {
+            // check if data is boxed, if yes then we cannot cast it directly
+            if (typeof(T).IsValueType)
+            {
+                return (T)Convert.ChangeType(data, typeof(T));
+            }
+
             return (T)data;
         }
     }
