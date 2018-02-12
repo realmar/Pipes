4d53cbc2f33d499c142b6124708dfc7b280431e8
Anastassios Martakos <anastassios.martakos@gmail.com>
2018-02-12 21:39:07 +0100

    Remove Process overload from ParallelProcessStrategy

--- Realmar.Pipes/ProcessStrategies/ParallelProcessStrategy.cs
+++ Realmar.Pipes/ProcessStrategies/ParallelProcessStrategy.cs
@@ -6,11 +6,6 @@ namespace Realmar.Pipes.ProcessStrategies
 {
 	public class ParallelProcessStrategy : IProcessStrategy
 	{
-		public void Process<TIn>(IPipe<TIn> pipe, TIn data)
-		{
-			pipe.FirstConnector.Process(data);
-		}
-
 		public void Process<TIn>(IPipeConnector<TIn> pipeConnector, IList<TIn> data)
 		{
 			Parallel.ForEach(data, pipeConnector.Process);
