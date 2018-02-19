c3bc14534926c8683a176478aa4a68f34958b705
Anastassios Martakos <anastassios.martakos@gmail.com>
2018-02-20 00:07:57 +0100

    Use InRange assertion in WaitProcessorTests.Process_CheckTime

--- Realmar.Pipes.Tests/Unit/Processors/Misc/WaitProcessorTests.cs
+++ Realmar.Pipes.Tests/Unit/Processors/Misc/WaitProcessorTests.cs
@@ -4,20 +4,20 @@ using Xunit;
 
 namespace Realmar.Pipes.Tests.Unit.Processors.Misc
 {
-    public class WaitProcessorTests
-    {
-        [Fact]
-        public void Process_CheckTime()
-        {
-            var waitTime = 500;
-            var processor = new WaitProcessor<object>(waitTime);
+	public class WaitProcessorTests
+	{
+		[Fact]
+		public void Process_CheckTime()
+		{
+			var waitTime = 500;
+			var processor = new WaitProcessor<object>(waitTime);
 
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-            processor.Process(new object());
-            stopwatch.Stop();
+			var stopwatch = new Stopwatch();
+			stopwatch.Start();
+			processor.Process(new object());
+			stopwatch.Stop();
 
-            Assert.True(stopwatch.Elapsed.TotalMilliseconds < waitTime * 1.1);
-        }
-    }
+			Assert.InRange(stopwatch.Elapsed.TotalMilliseconds, waitTime, waitTime * 1.5);
+		}
+	}
 }
